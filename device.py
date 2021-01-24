ee1d03bf94d7b0ea0366034bcc39e56cdfe9097e
wangdongyang <ustcdyw@163.com>
2021-01-24 20:53:39 +0800

    加入try catch

--- device.py
+++ device.py
@@ -5,15 +5,18 @@ import subprocess
 
 devices = []
 cmd = 'adb devices -l'
-res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-for line in res.stdout.readlines():
-    index = line.find(b'device product')
-    if index > 0:
-        info = line.split(sep=b':')
-        devID = info[0].split()[0].decode()
-        devDesc = (info[1] + info[2] + info[3].split()[0]).decode()
-        transportID = int(info[4].split(b'\r\n')[0].decode())
-        dev = {'devID': devID, 'devDesc': devDesc, 'transportID': transportID}
-        devices.append(dev)
-print(devices)
-res.stdout.close()
+try:
+    res = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in res.stdout.readlines():
+        index = line.find(b'device product')
+        if index > 0:
+            info = line.split(sep=b':')
+            devID = info[0].split()[0].decode()
+            devDesc = (info[1] + info[2] + info[3].split()[0]).decode()
+            transportID = int(info[4].split(b'\r\n')[0].decode())
+            dev = {'devID': devID, 'devDesc': devDesc, 'transportID': transportID}
+            devices.append(dev)
+    print(devices)
+    res.stdout.close()
+except expression as identifier:
+    pass
